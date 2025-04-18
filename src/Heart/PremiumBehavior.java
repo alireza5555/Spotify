6735b36987ec7a545477caafa532fb61791c2ff7
Alireza <rostmi83@gmail.com>
2025-04-18 10:59:03 +0330

    initialize premium behavior

---
+++ src/Heart/PremiumBehavior.java
@@ -0,0 +1,21 @@
+package Heart;
+
+public class PremiumBehavior implements UserBehavior{
+
+    int month;
+
+    @Override
+    public void createPlaylist(String Title, User Owner) {
+
+    }
+
+    @Override
+    public void playMusic(Music music) {
+
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) {
+        this.month += month;
+    }
+}
